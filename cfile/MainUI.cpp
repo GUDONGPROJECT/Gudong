9efe99b80a6737fdfbd5d42c019dc0be983170b4
Awaitzt <32785725+Awaitzt@users.noreply.github.com>
2018-10-18 12:00:36 +0800

    测试

--- cfile/MainUI.cpp
+++ cfile/MainUI.cpp
@@ -173,11 +173,11 @@ State SportMain(MOUSE *mouse,PEOPLE *people) {
 /**********************************************************
 Function：		Draw_Sport_Run
 
-Description：	画运动的主界面
+Description：	画跑步的主界面
 
 Input：			NONE
 
-Output：		运动主界面
+Output：		跑步主界面
 
 Return：		NONE
 **********************************************************/
@@ -218,11 +218,11 @@ void Draw_Sport_Run(void) {
 /**********************************************************
 Function：		Draw_Sport_Walk
 
-Description：	画运动的主界面
+Description：	画健走的主界面
 
 Input：			NONE
 
-Output：		运动主界面
+Output：		健走主界面
 
 Return：		NONE
 **********************************************************/
@@ -263,11 +263,11 @@ void Draw_Sport_Walk(void) {
 /**********************************************************
 Function：		Draw_Sport_Walk
 
-Description：	画运动的主界面
+Description：	画骑行的主界面
 
 Input：			NONE
 
-Output：		运动主界面
+Output：		骑行主界面
 
 Return：		NONE
 **********************************************************/
@@ -308,4 +308,106 @@ void Draw_Sport_Ride(void) {
     dis_16hz(47 * size - 16, 135 * size - 8, "干货", LIGHT_GRAY);
     dis_16hz(60 * size - 16, 135 * size - 8, "我的", LIGHT_GRAY);
 }
+/**********************************************************
+Function：		Draw_Mine
+
+Description：	画我的的主界面
+
+Input：			NONE
+
+Output：		我的主界面
+
+Return：		NONE
+**********************************************************/
+void Draw_Mine(PEOPLE &people){
+    float size=5.5;
+    char times[10],len[15];
+    itoa(people.exeTimes,times,10);
+    itoa(people.exeLen,len,10);
+    //将len转换为浮点公里整数与小数部分
+    char xiao[3];
+    for(int i=0;i<2;i++)
+        xiao[i]=len[strlen(len)-3+i];
+    xiao[2]='\0';
+    len[strlen(len)-3]='\0';
+    SVGA_Bar(0,0,68*size,140*size,WHITE);
+    //钱包按钮
+    dis_16hz(2*size,8*size,"钱包",BLACK);
+    //消息按钮
+    dis_16hz(66*size-32,8*size,"消息",BLACK);
+    //头像
+    SVGA_Ball(34*size,26*size,6*size,DARK_GRAY);
+    //用户名
+    dis_24zf(34*size-12*strlen(people.name),35*size-12,people.name,BLACK);
+    //分割线
+    SVGA_Line(3*size,40*size,65*size,40*size,DARK_GRAY);
+    //分割条
+    SVGA_Bar(0,58*size,68*size,60*size,LIGHT_GRAY);
+    //画运动记录按钮(0,60*size,68*size,70*size)
+    dis_24zf(4*size,65*size,times,BLACK);
+    dis_24hz(4*size+strlen(times)*24,65*size,"次运动",BLACK);
+    dis_16hz(4*size,70*size,"最近运动",LIGHT_GRAY);
+    dis_16zf(4*size+5*16,70*size,len,LIGHT_GRAY);
+    SVGA_Ball(4*size+(5+strlen(len))*16+8,70*size+14,2,LIGHT_GRAY);
+    dis_16zf(4*size+(6+strlen(len))*16,70*size,xiao,LIGHT_GRAY);
+    dis_16hz(4*size+(6+strlen(len)+strlen(xiao))*16,70*size,"公里",LIGHT_GRAY);
+    dis_16hz(50*size,68.5*size-8,"查看记录",LIGHT_GRAY);
+    SVGA_Line(66*size,68.5*size,65*size,67.5*size,DARK_GRAY);
+    SVGA_Line(66*size,68.5*size,65*size,69.5*size,DARK_GRAY);
+    //分割条
+    SVGA_Bar(0,77*size,68*size,79*size,LIGHT_GRAY);
+    //画设置按钮(0,60*size,68*size,70*size)
+    dis_16hz(4*size,84*size-8,"设置",BLACK);
+    SVGA_Line(66*size,84*size,65*size,83*size,DARK_GRAY);
+    SVGA_Line(66*size,84*size,65*size,85*size,DARK_GRAY);
+    //分割条
+    SVGA_Bar(0,89*size,68*size,130*size,LIGHT_GRAY);
+    //Simple setting(0,60*size,68*size,70*size,"设置");
+    //底部5个按钮
+    SVGA_Bar(0, 130 * size, 68 * size, 140 * size, WHITE);
+    dis_16hz(8 * size - 8 * 3, 135 * size - 8, "运动圈", LIGHT_GRAY);
+    dis_16hz(21 * size - 16, 135 * size - 8, "发现", LIGHT_GRAY);
+    dis_16hz(34 * size - 16, 135 * size - 8, "运动", GREEN);
+    dis_16hz(47 * size - 16, 135 * size - 8, "干货", LIGHT_GRAY);
+    dis_16hz(60 * size - 16, 135 * size - 8, "我的", LIGHT_GRAY);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
